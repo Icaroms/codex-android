61b46692441ce78959249f4defd231576ae871e7
Icaroms <icaromsmathius@gmail.com>
2026-06-10 00:14:40 -0300

    feat: adiciona ficha de jogo com column e row

--- app/src/main/java/br/com/icaroms/codex/MainActivity.kt
+++ app/src/main/java/br/com/icaroms/codex/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.icaroms.codex.ui.theme.CodexTheme
 
 class MainActivity : ComponentActivity() {
@@ -60,4 +64,31 @@ fun SaudacaoCodexPreview() {
 @Composable
 fun TituloCodex() {
     Text(text = "◆ CODEX ◆")
+}
+
+@Composable
+fun FichaDoJogo(nome: String, nota: Double, ano: Int) {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text( text = nome)
+        Row {
+            Text(text = "Nota: $nota")
+            Text(text = " - Ano: $ano")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FichaDoJogoPreview() {
+    FichaDoJogo(nome = "Hollow Knight", nota = 9.4, ano = 2017)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GaleriaCodex(){
+    Column(modifier = Modifier.padding(16.dp)) {
+        TituloCodex()
+        FichaDoJogo("Kenshi", 10.0, 2011)
+        FichaDoJogo("Dragon Age: Origins", 9.9, 2009)
+    }
 }
