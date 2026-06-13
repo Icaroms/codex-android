799239361b8c072e9caebbb42c09f967e9dc5eb1
Icaroms <icaromsmathius@gmail.com>
2026-06-13 19:43:36 -0300

    feat: adiciona botao de favorito com estado

--- app/src/main/java/br/com/icaroms/codex/MainActivity.kt
+++ app/src/main/java/br/com/icaroms/codex/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,6 +15,10 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -175,4 +180,22 @@ fun ListaComTitulo() {
 @Composable
 fun ListaComTituloPreview() {
     ListaComTitulo()
+}
+
+@Composable
+fun BotaoFavorito(nomeJogo: String) {
+    var favoritado by remember {mutableStateOf(false)}
+
+    Text(
+        text = if(favoritado) "★ Favoritado" else "☆ $nomeJogo",
+        modifier = Modifier
+            .padding(16.dp)
+            .clickable {favoritado = !favoritado}
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BotaoFavoritoPreview() {
+    BotaoFavorito("Hades")
 }
